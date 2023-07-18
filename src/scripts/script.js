4dfefd70048b8efc21fc86a7804b46c835657c3d
unknown <youssefhoussein701@gmail.com>
2023-07-19 00:16:32 +0300

    re-design the form to contain expense only

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,32 +1,34 @@
 const expense = $("#expense")
-const income = $("#income")
 const btn_submit = $("#submit")
-const name_text = $("#name")
-const amount_text =$("#amount")
+
+function addExpense(name,amount){
+    return `<table>
+    <td class="name">${name}</td>
+    <td class="amount">: ${amount}</td>
+  </table>`
+}
+
+
+
 expense.click(function(){
     const name_input = $("#input-name")
     const amount_input = $("#input-amount");
-    
-    
+    const exp_table = $("#expenses")
     btn_submit.show(200)
+    
     if(name_input.val().trim() === "" || amount_input.val().trim() === ""){
         return
     }
-    name_text.text(name_input.val())
-    amount_text.append(": -"+amount_input.val()+" $ <br>")
+    
+    
+    const table = $(addExpense(name_input.val(),amount_input.val()))
+    
+    exp_table.append(table)
+    
+    
+    
+    
     name_input.val("")
     amount_input.val("")
 })
-income.click(function(){
-    const name_input = $("#input-name")
-    const amount_input = $("#input-amount");
-    btn_submit.show(200)
-    if(name_input.val().trim() === "" || amount_input.val().trim() === ""){
-        return
-    }
-    name_text.text(name_input.val())
-    amount_text.append(": +"+amount_input.val()+" $ <br>")
 
-    name_input.val("")
-    amount_input.val("")
-})

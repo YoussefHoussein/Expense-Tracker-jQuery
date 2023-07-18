613bff13f377f7755305ce6a55c8097a599e70ef
unknown <youssefhoussein701@gmail.com>
2023-07-19 01:34:45 +0300

    add the delete expense and modify the total

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,9 +5,9 @@ let count_exp =0
 let display_value =0
 function addExpense(name,amount){
     return `<table>
-    <td class="name">${name}</td>
-    <td class="amount">:${amount}</td>
     <td><span class="remove">&#128465</span></td>
+    <td class="name">${name}</td>
+    <td class="amount">${amount}</td>
   </table>`
 }
 
@@ -27,11 +27,16 @@ expense.click(function(){
     const table = $(addExpense(name_input.val(),amount_input.val()))
     
     
-     exp_table.append(table)
+    exp_table.append(table)
      
-     let amount_val = parseFloat(amount_input.val())
-     count_exp += amount_val
-     
+    let amount_val = parseFloat(amount_input.val())
+    count_exp += amount_val
+    
+    table.find(".remove").click(function(){
+        console.log(table.find(".amount").text().trim())
+        changeTotal(table.find(".amount").text().trim())
+        table.remove()
+    })
     
     
     
@@ -40,10 +45,16 @@ expense.click(function(){
     amount_input.val("")
 })
 btn_calculate.click(function(){
-    console.log(count_exp)
+    
     display_value+=count_exp
    
      total.text(display_value)
      count_exp=0
  })
+ function changeTotal(value){
+    
+    display_value -=parseInt(value)
+    total.text(display_value)
+
+ }
 

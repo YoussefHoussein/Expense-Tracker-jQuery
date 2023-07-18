2b5c9de9af90b772395008556cf8c60d91ddfb06
unknown <youssefhoussein701@gmail.com>
2023-07-19 01:20:18 +0300

    add the total expense

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,13 @@
 const expense = $("#expense")
-const btn_submit = $("#submit")
-
+const btn_calculate = $("#calculate")
+const total =$("#exp_total")
+let count_exp =0
+let display_value =0
 function addExpense(name,amount){
     return `<table>
     <td class="name">${name}</td>
-    <td class="amount">: ${amount}</td>
+    <td class="amount">:${amount}</td>
+    <td><span class="remove">&#128465</span></td>
   </table>`
 }
 
@@ -14,7 +17,7 @@ expense.click(function(){
     const name_input = $("#input-name")
     const amount_input = $("#input-amount");
     const exp_table = $("#expenses")
-    btn_submit.show(200)
+    btn_calculate.show(200)
     
     if(name_input.val().trim() === "" || amount_input.val().trim() === ""){
         return
@@ -23,7 +26,12 @@ expense.click(function(){
     
     const table = $(addExpense(name_input.val(),amount_input.val()))
     
-    exp_table.append(table)
+    
+     exp_table.append(table)
+     
+     let amount_val = parseFloat(amount_input.val())
+     count_exp += amount_val
+     
     
     
     
@@ -31,4 +39,11 @@ expense.click(function(){
     name_input.val("")
     amount_input.val("")
 })
+btn_calculate.click(function(){
+    console.log(count_exp)
+    display_value+=count_exp
+   
+     total.text(display_value)
+     count_exp=0
+ })
 
